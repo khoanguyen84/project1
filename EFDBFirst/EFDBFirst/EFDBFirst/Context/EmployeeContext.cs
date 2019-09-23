70a528a85a199373a973f8bee3cce4592e2c70cd
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2019-09-23 13:43:36 +0700

    EF DB First

---
+++ EFDBFirst/EFDBFirst/EFDBFirst/Context/EmployeeContext.cs
@@ -0,0 +1,22 @@
+﻿using EFDBFirst.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFDBFirst.Context
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+        public DbSet<tblSkill> tblSkills { get; set; }
+        public DbSet<tblEmployee> tblEmployees { get; set; }
+        public DbSet<EmployeeViewModel> EmployeeViewModel { get; set; }
+        public DbSet<EFDBFirst.Models.EmployeeCreateModel> EmployeeCreateModel { get; set; }
+        public DbSet<EFDBFirst.Models.EmployeeEditModel> EmployeeEditModel { get; set; }
+    }
+}
